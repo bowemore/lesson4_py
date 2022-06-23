dce5075ebc0a75748c845d927e2c8dde5b6d1ef0
walle <walle@123.ru>
2022-06-23 16:32:07 +0300

    added args

--- homework.py
+++ homework.py
@@ -1,8 +1,19 @@
-def open_browser():
+
+def string_name(func):
+    print('Имя функции:\n', '',  func.__name__ )
+    if func.__code__.co_varnames:
+        print('Аргументы функции:')
+    else:
+        print('Аргументов нет')
+    for arg in func.__code__.co_varnames:
+        print('  ' + arg)
+
+
+def open_browser(browser, version):
     pass
 
 
-def go_to_companyname_homepage():
+def go_to_companyname_homepage(company_name, region):
     pass
 
 
@@ -10,7 +21,6 @@ def find_registration_button_on_login_page():
     pass
 
 
-i = (open_browser, go_to_companyname_homepage, find_registration_button_on_login_page)
-for s in i:
-    string_name = s.__name__
-    print(string_name)
+functions = (open_browser, go_to_companyname_homepage, find_registration_button_on_login_page)
+for s in functions:
+    string_name(s)
